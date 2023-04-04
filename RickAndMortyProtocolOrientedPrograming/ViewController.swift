e50afe1a0cd2842381de62ccdc680a27166257b3
Muhammed Emin Yildirim <yldrmmuhammedemin@gmail.com>
2023-04-04 16:11:09 +0300

    Initial Commit

---
+++ RickAndMortyProtocolOrientedPrograming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyProtocolOrientedPrograming
+//
+//  Created by Yildirim on 4.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RickAndMortyProtocolOrientedProgramingUITests/RickAndMortyProtocolOrientedProgramingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RickAndMortyProtocolOrientedProgramingUITestsLaunchTests.swift
+//  RickAndMortyProtocolOrientedProgramingUITests
+//
+//  Created by Yildirim on 4.04.2023.
+//
+
+import XCTest
+
+final class RickAndMortyProtocolOrientedProgramingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
